ecbc5b43f65ca46941e60872b075a379106ac7ad
Timothy Chung <timchunght@gmail.com>
2016-11-21 16:27:10 -0500

    initial commit

---
+++ scripts/demo.py
@@ -0,0 +1,193 @@
+#!/usr/bin/env python
+import copy
+import actionlib
+import rospy
+
+from math import sin, cos
+from moveit_python import (MoveGroupInterface,
+                           PlanningSceneInterface,
+                           PickPlaceInterface)
+from moveit_python.geometry import rotate_pose_msg_by_euler_angles
+
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
+from control_msgs.msg import PointHeadAction, PointHeadGoal
+from grasping_msgs.msg import FindGraspableObjectsAction, FindGraspableObjectsGoal
+from geometry_msgs.msg import PoseStamped
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from moveit_msgs.msg import PlaceLocation, MoveItErrorCodes
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+
+
+
+
+# Tools for grasping
+class GraspingClient(object):
+
+    def __init__(self):
+        self.scene = PlanningSceneInterface("base_link")
+        self.pickplace = PickPlaceInterface("arm", "gripper", verbose=True)
+        self.move_group = MoveGroupInterface("arm", "base_link")
+
+        find_topic = "basic_grasping_perception/find_objects"
+        rospy.loginfo("Waiting for %s..." % find_topic)
+        self.find_client = actionlib.SimpleActionClient(find_topic, FindGraspableObjectsAction)
+        self.find_client.wait_for_server()
+
+    def updateScene(self):
+        # find objects
+        goal = FindGraspableObjectsGoal()
+        goal.plan_grasps = True
+        self.find_client.send_goal(goal)
+        self.find_client.wait_for_result(rospy.Duration(5.0))
+        find_result = self.find_client.get_result()
+
+        """
+        might be useful if we want to pick and place multiple objects
+        """
+        # remove previous objects
+        for name in self.scene.getKnownCollisionObjects():
+            self.scene.removeCollisionObject(name, False)
+        for name in self.scene.getKnownAttachedObjects():
+            self.scene.removeAttachedObject(name, False)
+        self.scene.waitForSync()
+
+        # insert objects to scene
+        idx = -1
+        for obj in find_result.objects:
+            idx += 1
+            obj.object.name = "object%d"%idx
+            self.scene.addSolidPrimitive(obj.object.name,
+                                         obj.object.primitives[0],
+                                         obj.object.primitive_poses[0],
+                                         wait = False)
+
+        for obj in find_result.support_surfaces:
+            # extend surface to floor, and make wider since we have narrow field of view
+            height = obj.primitive_poses[0].position.z
+            obj.primitives[0].dimensions = [obj.primitives[0].dimensions[0],
+                                            1.5,  # wider
+                                            obj.primitives[0].dimensions[2] + height]
+            obj.primitive_poses[0].position.z += -height/2.0
+
+            # add to scene
+            self.scene.addSolidPrimitive(obj.name,
+                                         obj.primitives[0],
+                                         obj.primitive_poses[0],
+                                         wait = False)
+
+        self.scene.waitForSync()
+
+        # store for grasping
+        self.objects = find_result.objects
+        self.surfaces = find_result.support_surfaces
+
+    def getGraspableCube(self):
+        graspable = None
+        for obj in self.objects:
+            # need grasps
+            if len(obj.grasps) < 1:
+                continue
+            # check size
+            if obj.object.primitives[0].dimensions[0] < 0.05 or \
+               obj.object.primitives[0].dimensions[0] > 0.07 or \
+               obj.object.primitives[0].dimensions[0] < 0.05 or \
+               obj.object.primitives[0].dimensions[0] > 0.07 or \
+               obj.object.primitives[0].dimensions[0] < 0.05 or \
+               obj.object.primitives[0].dimensions[0] > 0.07:
+                continue
+            # has to be on table
+            if obj.object.primitive_poses[0].position.z < 0.5:
+                continue
+            return obj.object, obj.grasps
+        # nothing detected
+        return None, None
+
+    def getSupportSurface(self, name):
+        for surface in self.support_surfaces:
+            if surface.name == name:
+                return surface
+        return None
+
+    def getPlaceLocation(self):
+        pass
+
+    def pick(self, block, grasps):
+        success, pick_result = self.pickplace.pick_with_retry(block.name,
+                                                              grasps,
+                                                              support_name=block.support_surface,
+                                                              scene=self.scene)
+        self.pick_result = pick_result
+        return success
+
+    def place(self, block, pose_stamped):
+        places = list()
+        l = PlaceLocation()
+        l.place_pose.pose = pose_stamped.pose
+        l.place_pose.header.frame_id = pose_stamped.header.frame_id
+
+        """
+        can change the place location based on the pick location. Let the pick location be the origin.
+        """
+        # copy the posture, approach and retreat from the grasp used
+        l.post_place_posture = self.pick_result.grasp.pre_grasp_posture
+        l.pre_place_approach = self.pick_result.grasp.pre_grasp_approach
+        l.post_place_retreat = self.pick_result.grasp.post_grasp_retreat
+        places.append(copy.deepcopy(l))
+        # create another several places, rotate each by 360/m degrees in yaw direction
+        m = 16 # number of possible place poses
+        pi = 3.141592653589
+        for i in range(0, m-1):
+            l.place_pose.pose = rotate_pose_msg_by_euler_angles(l.place_pose.pose, 0, 0, 2 * pi / m)
+            places.append(copy.deepcopy(l))
+
+        success, place_result = self.pickplace.place_with_retry(block.name,
+                                                                places,
+                                                                scene=self.scene)
+        return success
+
+"""
+We may need to change the shape of the table in the config file according to the real experiment 
+"""
+
+
+
+
+if __name__ == "__main__":
+    # Create a node
+    rospy.init_node("demo")
+
+    # Make sure sim time is working
+    while not rospy.Time.now():
+        pass
+
+    # Setup clients
+    grasping_client = GraspingClient()
+    """
+    Add the object recognitio we used (RANSAC)
+    Add the grapit! plugin
+    """
+    # Get block to pick
+    while not rospy.is_shutdown():
+        rospy.loginfo("Picking object...")
+        grasping_client.updateScene()
+        cube, grasps = grasping_client.getGraspableCube()
+        if cube == None:
+            rospy.logwarn("Perception failed.")
+            continue
+
+        # Pick the block
+        if grasping_client.pick(cube, grasps):
+            break
+        rospy.logwarn("Grasping failed.")
+
+    # Place the block
+    while not rospy.is_shutdown():
+        rospy.loginfo("Placing object...")
+        pose = PoseStamped()
+        pose.pose = cube.primitive_poses[0]
+        pose.pose.position.z += 0.05
+        pose.header.frame_id = cube.header.frame_id
+        if grasping_client.place(cube, pose):
+            break
+        rospy.logwarn("Placing failed.")
